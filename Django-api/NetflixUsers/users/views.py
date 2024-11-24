c6737335be765af3e8951c12fca1271c9dc96953
hpeacant <hpeacant@alumnos.unex.es>
2024-11-24 17:29:39 +0100

    GC04-7 UC07 - User consultation

--- Django-api/NetflixUsers/users/views.py
+++ Django-api/NetflixUsers/users/views.py
@@ -1,6 +1,14 @@
-from rest_framework import viewsets
+from django.db.models import Q
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from .models import PaymentMethod, User
 from .serializers import PaymentMethodSerializer, UserSerializer
+
+
+def get_email_filter(email):
+    if not email:
+        return Q()
+    return Q(email=email)
 
 
 class PaymentMethodViewSet(viewsets.ModelViewSet):
@@ -13,3 +21,10 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = "id"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request):
+        email = request.query_params.get("email")
+        filters = get_email_filter(email)
+        filtered_users = self.queryset.filter(filters)
+        serializer = self.serializer_class(filtered_users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
